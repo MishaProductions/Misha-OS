47ff90058659cbfd5968deb53480d3170e121d13
MishaTY <46088515+MishaTY@users.noreply.github.com>
2021-04-28 18:24:09 -0400

    Bump version

--- Misha-OS/Kernel.cs
+++ Misha-OS/Kernel.cs
@@ -13,7 +13,7 @@ namespace MishaOS
     {
         public static Sys.FileSystem.CosmosVFS FS;
 
-        public static string KernelVersion = "MishaOS Version 0.9b";
+        public static string KernelVersion = "MishaOS Version 0.9c";
         protected override void BeforeRun()
         {
             try
